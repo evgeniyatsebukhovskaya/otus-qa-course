4aebf3c05d86929a2576593e3814ed210c8fc602
Evgeniya <e.tsebukhovskaya@gmail.com>
2019-08-14 23:45:19 +0700

    inserted fixtures to conftest

--- test/tests_task1.py
+++ test/tests_task1.py
@@ -1,23 +1,5 @@
 import pytest
 
-@pytest.fixture(scope="session")
-def session_fixture():
-    print("It is session fixture")
-    yield
-    print("Finalizer from session fixture")
-
-@pytest.fixture(scope="module")
-def module_fixture(request):
-    print("It is module fixture")
-    def fin():
-        print("Finilizer from module fixture")
-
-    request.addfinalizer(fin)
-
-@pytest.fixture()
-def function_fixture():
-    print("It is function fixture")
-
 
 """Проверяем, что функция len возвращает длину списка"""
 
